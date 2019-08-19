4e6f950888d367da8a596e6b10b6772e04bbcda3
Cristiano Carvalho Amaral <cacofut@hotmail.com>
2019-08-19 12:54:15 -0300

    Primeiro commit

---
+++ src/main/java/br/com/cacodev/TacoCloudJpaApplication.java
@@ -0,0 +1,13 @@
+package br.com.cacodev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TacoCloudJpaApplication.class, args);
+    }
+
+}
